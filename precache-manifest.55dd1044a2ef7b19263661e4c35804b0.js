603bd2bb2164ef34d55e14568ad4393f5bc9c0e0
Alexmaingi <alexmaingi077@gmail.com>
2022-05-19 00:29:10 -0700

    Updates

---
+++ precache-manifest.55dd1044a2ef7b19263661e4c35804b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "578a4e69e6a1a7e20bca",
+    "url": "/todo-app/static/css/main.fad78a0f.chunk.css"
+  },
+  {
+    "revision": "578a4e69e6a1a7e20bca",
+    "url": "/todo-app/static/js/main.578a4e69.chunk.js"
+  },
+  {
+    "revision": "0e02775030053b0d2432",
+    "url": "/todo-app/static/js/1.0e027750.chunk.js"
+  },
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "adfed087566968e5660be5ee2593dfde",
+    "url": "/todo-app/index.html"
+  }
+];
